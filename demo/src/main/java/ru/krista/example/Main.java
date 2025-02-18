ab9a8c310068912a70f46b5a6e48612fe683bcf5
1lonMusk-ai~ <term24@debian.classroom.local>
2025-02-18 11:27:47 +0300

    Initial commit~

---
+++ demo/src/main/java/ru/krista/example/Main.java
@@ -0,0 +1,8 @@
+package ru.krista.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
